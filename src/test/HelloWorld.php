46d31a38f84f38557567014f369af6d0bed2c0b9
Breezess <s88858@163.com>
2014-01-28 13:50:57 +0800

    test

---
+++ src/test/HelloWorld.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * HelloWorld for demonstration purpose.
+ *
+ * Authors:
+ *  Albert Bakker (hallo[at]abbert[dot]nl)
+ *
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ */
+namespace HelloWorld;
+
+/**
+ * Class HelloWorld
+ * @package HelloWorld
+ */
+class HelloWorld
+{
+	/**
+	 * @return string
+	 */
+	public function sayHello()
+	{
+		return 'Hello World!';
+	}
+}
