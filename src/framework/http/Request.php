ca04733fcfc5b0d8f11584fbe4a0e107105f366b
Breezess <s88858@163.com>
2014-01-29 11:17:14 +0800

    test

---
+++ src/framework/http/Request.php
@@ -0,0 +1,10 @@
+<?php 
+namespace framework\http;
+
+class Request
+{
+    public function __construct()
+    {
+        echo __CLASS__ . " constructer!";
+    }
+} 
---
+++ src/framework/http/Response.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace framework\http;
+
+class Response
+{            
+    public function __construct()
+    {      
+        echo __CLASS__ . " constructed!";                
+    }           
+}   
